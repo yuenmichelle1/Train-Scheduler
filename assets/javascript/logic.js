24889bed6c03c1a8b0dcda050e1519de49ae9cf8
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-01-15 14:16:15 -0600

    finishing logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -29,4 +29,53 @@ var database= firebase.database();
     }
     //upload new train data into Firebase
     database.ref().push(newTrain);
+
+    //Clear all of user input once submitted into Firebase
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+    })
+
+    //Create firebase event when child is added
+    database.ref().on("child_added", function(snapshot, prevChildKey){
+        var tName = snapshot.val().name;
+        var tDestination = snapshot.val().destination;
+        var tTime= snapshot.val().time;
+        var tFreq= snapshot.val().frequency;
+        console.log(tTime);
+
+        //Moment Math
+        //Current Time: 
+        var currentTime = moment();
+        console.log(currentTime);
+        //format current time
+        var formattedCurrentTime= moment(currentTime).format("hh:mm");
+        //convert firstTrainTime given
+        var tStart = moment(tTime, "hh:mm").subtract(1,"years");
+        console.log(tStart);
+
+        //Difference in time
+        var timeDifference = currentTime.diff(moment(tStart), "minutes");
+        console.log(timeDifference);
+
+        //remainder time
+        var remainder= timeDifference%tFreq;
+
+        var nextTrainMin= tFreq - remainder;
+
+        var nextTrain = currentTime.add(nextTrainMin, "minutes").format("hh:mm");
+        //add new Row in table
+        var tRow = $("<tr>");
+
+        tRow.append(`<td> ${tName} </td>`);
+        tRow.append(`<td> ${tDestination}</td>`);
+        tRow.append(`<td>${tFreq}</td>`);
+        tRow.append(`<td>${nextTrain}</td>`);
+        tRow.append(`<td>${nextTrainMin}</td>`);
+
+        $("#train-table").append(tRow);
+
+
+
     })
